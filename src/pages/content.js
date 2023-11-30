361500088cc4b676395ec76248f2c7bcf75c80c3
Viraj Shirodkar <virajshirodkar45@gmail.com>
2023-11-30 09:01:47 +0530

    footer updates

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,153 +1,157 @@
 // Personal Information --> #hero section
-export const name = "Peter Griffin"
-export const designation = "Safety Inspector"
-export const location = "Quahog, Rhode Island"
-export const pronouns = "he/him"
-export const website = "https://petergriffin.com"
+export const name = "Viraj Shirodkar";
+export const designation = "Engineering Studen";
+export const location = "Mumbai, India";
+export const pronouns = "he/him";
+export const website = "https://linuxcoder.netlify.app";
 
 // About
-export const about = "An adventurous man working as a safety inspector with a quirky sense of humor. Husband to Louis Griffin. Father of Chris, Brian, and Stewie."
+export const about =
+  "Computer Science student focusing on AI and ML, skilled in Python, C++, Java, and web development (HTML, CSS, Bootstrap, Tailwind CSS, WordPress). Strengthening proficiency in C++ and DSA. Passionate about tech innovation, seeking collaborative projects in Computer Science, AI, and programming.";
 
 // Work Experience --> #work section
 export const work = [
-	{
-		from: "2001",
-		to: "Present",
-		role: "Safety Inspector",
-		org: "Quahog Safety Department",
-		org_url: "https://quahog.gov/safety",
-		location: "Quahog, Rhode Island",
-		skills: "Safety Inspection, Risk Assessment, Emergency Response"
-	}
-]
+  {
+    from: "2023",
+    to: "Present",
+    role: "Ongoing Education in Computer Science",
+    org: "DY Patil - RAIT",
+    org_url: "https://dypatil.edu/",
+    location: "Nerul, Navi Mumbai",
+    skills: "BTech CSE - ( Artificial Intelligence and Machine Learning )",
+  },
+];
 
 // Projects --> #project section
 export const projects = [
-	{
-		title: "Project Griffin",
-		description: "A project to improve safety standards in Quahog by implementing innovative safety measures.",
-		url: "https://projectgriffin.com",
-		tech: "Safety Equipment, Training Programs, Community Outreach",
-	},
-	{
-		title: "Family Safety Campaign",
-		description: "An initiative to promote safety awareness among families in Quahog.",
-		url: "https://safefamilyquahog.com",
-		tech: "Workshops, Educational Material, Public Events",
-	},
-	{
-		title: "Road Safety Initiative",
-		description: "A campaign to enhance road safety and reduce accidents in Quahog.",
-		url: "https://roadsafetyquahog.com",
-		tech: "Traffic Analysis, Signage Improvement, Public Awareness",
-	}
-]
+  {
+    title: "Portfolio Website",
+    description:
+      "Single-page portfolio website with contact and pricing details.",
+    url: "https://linux-coder.netlify.app/",
+    tech: "HTML, CSS, Bootstrap",
+  },
+  //   {
+  //     title: "Family Safety Campaign",
+  //     description:
+  //       "An initiative to promote safety awareness among families in Quahog.",
+  //     url: "https://safefamilyquahog.com",
+  //     tech: "Workshops, Educational Material, Public Events",
+  //   },
+  //   {
+  //     title: "Road Safety Initiative",
+  //     description:
+  //       "A campaign to enhance road safety and reduce accidents in Quahog.",
+  //     url: "https://roadsafetyquahog.com",
+  //     tech: "Traffic Analysis, Signage Improvement, Public Awareness",
+  //   },
+];
 
 // Education --> #education section
 export const education = [
-	{
-		from: "1995",
-		to: "1999",
-		institute: "Quahog High School",
-		course: "High School Diploma",
-		location: "Quahog, Rhode Island"
-	},
-	{
-		from: "1999",
-		to: "2003",
-		institute: "Quahog Community College",
-		course: "Bachelor of Science in Safety Management",
-		location: "Quahog, Rhode Island"
-	},
-	{
-		from: "2003",
-		to: "2005",
-		institute: "Safety University",
-		course: "Master of Science in Safety Engineering",
-		location: "Boston, Massachusetts"
-	}
-]
+  {
+    from: "2021",
+    to: "2023",
+    institute: "SIES High School",
+    course: "High School Diploma",
+    location: "Sion, Mumbai",
+  },
+  {
+    from: "2023",
+    to: "2027",
+    institute: "DY Patil - RAIT",
+    course: "BTech CSE - ( Artificial Intelligence and Machine Learning )",
+    location: "Nerul, Navi Mumbai",
+  },
+  // {
+  // 	from: "2003",
+  // 	to: "2005",
+  // 	institute: "Safety University",
+  // 	course: "Master of Science in Safety Engineering",
+  // 	location: "Boston, Massachusetts"
+  // }
+];
 
 // Contact --> #contact section
 export const contact = [
-	{
-		source_name: "Email",
-		source: "peter.griffin@example.com",
-	},
-	{
-		source_name: "Twitter",
-		source: "https://twitter.com/PeterGriffin",
-	},
-	{
-		source_name: "LinkedIn",
-		source: "https://www.linkedin.com/in/peter-griffin",
-	},
-]
+  {
+    source_name: "Email",
+    source: "virajshirodkar45@gmail.com",
+  },
+  {
+    source_name: "Twitter",
+    source: "https://twitter.com/V70550Viraj",
+  },
+  {
+    source_name: "LinkedIn",
+    source: "www.linkedin.com/in/viraj-shirodkar",
+  },
+];
 
 // Certifications --> #certificate section
 
 export const certificates = [
-	{
-		issued: "2005",
-		title: "Certified Safety Inspector",
-		org: "National Safety Association",
-		url: "https://certi.com/url",
-		description: "Certification in safety inspection and risk assessment.",
-	},
-	{
-		issued: "2006",
-		title: "Emergency Response Training",
-		org: "Quahog Safety Department",
-		url: "https://certi.com/url",
-		description: "Training in handling emergency situations and crisis management.",
-	},
-	{
-		issued: "2007",
-		title: "Safety Leadership Workshop",
-		org: "Safety Leaders International",
-		url: "https://certi.com/url",
-		description: "Leadership training in safety management and communication.",
-	}
-]
+  {
+    issued: "2023",
+    title: "C++ Course: Learn the Essentials",
+    org: "Scaler",
+    url: "https://media.licdn.com/dms/image/D4D22AQEbSNcoPddXXQ/feedshare-shrink_2048_1536/0/1693832766400?e=1704326400&v=beta&t=7AVqH_7kBNtKr13Xk4biGIQm_lTWpEkb0Yeqsqf9Ss8",
+    description: "Certification for completion of the tutorial.",
+  },
+  {
+    issued: "2023",
+    title: "DSA Workshop in C",
+    org: "GDSC - RAIT",
+    url: "https://mail.google.com/mail/u/0/popout?ver=1g3ytthb29lwb#attid%253Datt_18af134ef8f66d04_0.1_dc60cbc6_5a9b3874_81507438_7645c941_63810dd1%25252FViraj%252520Shirodkar.pdf",
+    description: "Successful completetion of workshop.",
+  },
+  // {
+  // 	issued: "2007",
+  // 	title: "Safety Leadership Workshop",
+  // 	org: "Safety Leaders International",
+  // 	url: "https://certi.com/url",
+  // 	description: "Leadership training in safety management and communication.",
+  // }
+];
 
 // Blogs --> #blogs section
 export const blogs = [
-	{
-		date: "12-July, 2023",
-		url: "https://medium.com/",
-		title: "Adventures in Safety Inspection",
-		description: "Join me in my humorous escapades as a safety inspector in Quahog.",
-		publisher: "Quahog Times",
-	},
-	{
-		date: "25-June, 2023",
-		url: "https://medium.com/",
-		title: "Top 10 Safety Tips for Families",
-		description: "Learn the best safety practices for families to ensure a secure environment.",
-		publisher: "Safety Monthly",
-	}
-]
+  // {
+  // 	date: "12-July, 2023",
+  // 	url: "https://medium.com/",
+  // 	title: "Adventures in Safety Inspection",
+  // 	description: "Join me in my humorous escapades as a safety inspector in Quahog.",
+  // 	publisher: "Quahog Times",
+  // },
+  // {
+  // 	date: "25-June, 2023",
+  // 	url: "https://medium.com/",
+  // 	title: "Top 10 Safety Tips for Families",
+  // 	description: "Learn the best safety practices for families to ensure a secure environment.",
+  // 	publisher: "Safety Monthly",
+  // }
+];
 
 // Achievements --> #achievement section
 export const achievements = [
-	{
-		title: "Safety Hero Award",
-		year: "2020",
-		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
-	},
-	{
-		title: "Safety Innovation Prize",
-		year: "2018",
-		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
-	},
-	{
-		title: "Community Safety Ambassador",
-		year: "2015",
-		decription: "Honored for promoting safety awareness and education in the Quahog community."
-	},
-	{
-		title: "Safety Campaign Excellence",
-		year: "2013",
-		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
-	}
-]
+  // {
+  // 	title: "Safety Hero Award",
+  // 	year: "2020",
+  // 	decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
+  // },
+  // {
+  // 	title: "Safety Innovation Prize",
+  // 	year: "2018",
+  // 	decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
+  // },
+  // {
+  // 	title: "Community Safety Ambassador",
+  // 	year: "2015",
+  // 	decription: "Honored for promoting safety awareness and education in the Quahog community."
+  // },
+  // {
+  // 	title: "Safety Campaign Excellence",
+  // 	year: "2013",
+  // 	decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
+  // }
+];
